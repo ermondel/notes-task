b758c555416e066ad7a7c64844d279f1d2df7305
ermondel <ermondel@gmail.com>
2021-02-02 21:20:39 +0200

    Add years to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 
-const Footer = () => (
-  <Container className="Footer">
-    <a
-      href="https://github.com/ermondel"
-      className="author"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      © Serhii
-    </a>
-  </Container>
-);
+const Footer = () => {
+  let footerYear = 2019;
+  const currentYear = new Date().getFullYear();
+
+  if (footerYear !== currentYear) {
+    footerYear = `${footerYear} - ${currentYear}`;
+  }
+
+  return (
+    <Container className='Footer'>
+      <span className='years'>&copy; {footerYear}</span>{' '}
+      <a
+        href='https://github.com/ermondel'
+        target='_blank'
+        rel='noopener noreferrer'
+        title='All rights reserved | created by Serhii (ermondel)'
+        className='author'
+      >
+        Serhii
+      </a>
+    </Container>
+  );
+};
 
 export default Footer;
